faeef586dc00e5cf0f96271fddbbcf59834cd13b
manucirne <manucirne@gmail.com>
2019-09-11 15:23:14 -0300

    APS1 funcionando

---
+++ tarefa_aps1.py
@@ -0,0 +1,49 @@
+from flask import Flask, request
+from flask_restful import Resource
+
+id_count = 1
+
+app = Flask(__name__)
+dados = {}
+
+
+class TarefaPorId(Resource):
+
+    def get(self, id): #mostra uma tarefa por id
+        global dados
+        if id in dados:
+            return dados[id], 200
+        return {'Erro':'Id inexistente!'},400    
+    
+
+    def delete(self, id): #deleta uma tarefa por id
+        global dados
+        try:
+            del dados[id]
+        except KeyError:
+            return {"Erro":"Id inexistente!"},400
+        return {"Sucesso":"Arrasou!"},200
+
+    def put(self, id):  #update de uma tarefa por id
+        global dados
+        if id in dados:
+            dados[id] = {}
+            dados[id] = request.get_json()
+            return {"Sucesso":"Arrasou!"},200
+        return {"Erro":"Id inexistente!"},400
+        
+
+
+class Tarefas(Resource):
+    def post(self):   #criatarefa
+        global dados
+        global id_count
+        dados[id_count] = request.get_json()
+        id_count += 1
+        return {"Sucesso":"Arrasou!"},200
+
+    def get(self): #mostra todas as tarfeas
+        global dados
+        if len(dados) > 0:
+            return dados, 200
+        return {"error":'empty'}, 400
---
+++ webserver.py
@@ -0,0 +1,18 @@
+#!flask/bin/python
+from flask import Flask, request
+from flask_restful import Api
+from tarefa_aps1 import TarefaPorId, Tarefas
+
+app = Flask(__name__)
+api = Api(app)
+
+api.add_resource(TarefaPorId, '/tarefa/<int:id>')
+api.add_resource(Tarefas, '/tarefa')
+
+@app.route('/healthcheck')
+def hc():
+    return '',200
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
