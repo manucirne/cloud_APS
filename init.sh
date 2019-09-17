6690ff13823c712567debd3b5844f694de70ad49
manucirne <manucirne@gmail.com>
2019-09-17 14:34:24 -0300

    init com aplicação inicializada

--- init.sh
+++ init.sh
@@ -1,8 +1,7 @@
 sudo apt-get update
-sudo apt-get python3
-sudo apt-get python3-pip
+sudo apt-get install python3
+sudo apt-get install python3-pip
 pip3 install flask
 pip3 install flask_restful
-cd cloud_APS
 export IPADDR="http://127.0.0.1:5000"
-
+python3 webserver.py
