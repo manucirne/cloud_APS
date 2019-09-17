6a08eed27771e4165a51e0815389ea8f35b6d16a
manucirne <manucirne@gmail.com>
2019-09-17 15:07:39 -0300

    correção no init.sh

--- init.sh
+++ init.sh
@@ -1,7 +1,7 @@
-sudo apt-get update
-sudo apt-get install python3
-sudo apt-get install python3-pip
-pip3 install flask
-pip3 install flask_restful
+sudo apt-get update -y
+sudo apt-get install python3 -y
+sudo apt-get install python3-pip -y
+pip3 install flask -y
+pip3 install flask_restful -y
 export IPADDR="http://127.0.0.1:5000"
 python3 webserver.py
